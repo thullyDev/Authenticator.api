4b355f200fceadeb4fbef8fb6522e94c37e81a08
thullyDev <thully.developer@gmail.com>
2024-06-02 04:23:47 +0200

    checkpointing

--- app/main.py
+++ app/main.py
@@ -1,11 +1,33 @@
 from fastapi import FastAPI
 from typing import Any, Dict
-from .routers import auth_router 
+# from .routers import auth_router 
+from fastapi.responses import JSONResponse
+from app.handlers.response_handler import ResponseHandler
 
 app = FastAPI()
+response: ResponseHandler = ResponseHandler()
+
+async def custom_middleware(request, call_next):
+    # if some_condition:
+        # Return a custom response
+    return JSONResponse({"message": "Custom response"}, status_code=200)
+
+    # response = await call_next(request)
+    # return response
+
+# Register the middleware with the FastAPI app
+app.middleware("http")(custom_middleware)
+
+@app.exception_handler(Exception)
+def unexpected_error_handler(request, exc) -> JSONResponse:
+    return response.crash_response(data={ "message": "Unexpected error occurred" })
 
 @app.get("/")
-def root() -> Dict[str, str]:
-    return {"Message": "server is running... follow me on https://github.com/thullDev"}
+def root() -> JSONResponse:
+    return response.successful_response(data={ "message": "server is running... follow me on https://github.com/thullDev" })
 
-app.include_router(auth_router.router)
+# app.include_router(auth_router.router)
+
+# if __name__ == "__main__":
+#     import uvicorn
+#     uvicorn.run(app, host=HOST, port=PORT)
--- app/routers/auth_router.py
+++ app/routers/auth_router.py
@@ -1,12 +1,33 @@
-from fastapi import APIRouter
-from fastapi.responses import JSONResponse
-from app.handlers.response_handler import ResponseHandler
-from app.resources.errors import CRASH
+# from fastapi import APIRouter, Depends
+# from fastapi.responses import JSONResponse
+# from app.handlers.response_handler import ResponseHandler
+# from app.resources.errors import CRASH
 
-router: APIRouter = APIRouter(prefix="/auth")
-response: ResponseHandler = ResponseHandler()
+# router: APIRouter = APIRouter(prefix="/auth")
+# response: ResponseHandler = ResponseHandler()
 
 
-@router.get("/signup")
-async def signup() -> JSONResponse:
-     return response.successful_response()
+# Custom middleware function
+# async def custom_middleware(request, call_next):
+    # Check if a specific condition is met
+    # if some_condition:
+        # Return a custom response
+        # return response.successful_response({"message": "Custom response"})
+
+    # If the condition is not met, continue with the request
+    # response = await call_next(request)
+    # return response
+
+# Register the middleware with the FastAPI app
+# router.middleware("http")(custom_middleware)
+
+# def verify_request():
+#      token = None
+     
+#      if not token:
+#           return {"status_code": 401, "detail": "Invalid token"}
+
+
+# @router.get("/signup")
+# def signup(token: str = Depends(verify_request)) -> JSONResponse:
+#      return response.successful_response()
